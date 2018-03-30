2fe83b1320be0965c3f7422e4a34218c7193af82
kaiser <kaiserqin.work@gmail.com>
2018-03-30 10:10:14 +0800

    修改版本号

---
+++ src/demo/demoData/picker.js
@@ -0,0 +1,349 @@
+const pickerData1 = [
+  {
+    text: '1',
+    value: '1',
+    id: '1'
+  },
+  {
+    text: '2',
+    value: '2',
+    id: '2'
+  },
+  {
+    text: '3',
+    value: '3',
+    id: '3'
+  },
+  {
+    text: '4',
+    value: '4',
+    id: '4'
+  },
+  {
+    text: '5',
+    value: '5',
+    id: '5'
+  },
+  {
+    text: '6',
+    value: '6',
+    id: '6'
+  },
+  {
+    text: '7',
+    value: '7',
+    id: '7'
+  },
+  {
+    text: '8',
+    value: '8',
+    id: '8'
+  },
+  {
+    text: '9',
+    value: '9',
+    id: '9'
+  }
+]
+
+const pickerData2 = [
+  {
+    text: '11',
+    value: '11',
+    id: '11'
+  },
+  {
+    text: '22',
+    value: '22',
+    id: '22'
+  },
+  {
+    text: '33',
+    value: '33',
+    id: '33'
+  },
+  {
+    text: '44',
+    value: '44',
+    id: '44'
+  },
+  {
+    text: '55',
+    value: '55',
+    id: '55'
+  },
+  {
+    text: '66',
+    value: '66',
+    id: '66'
+  },
+  {
+    text: '77',
+    value: '77',
+    id: '77'
+  },
+  {
+    text: '88',
+    value: '88',
+    id: '88'
+  },
+  {
+    text: '99',
+    value: '99',
+    id: '99'
+  }
+]
+
+const pickerData3 = [
+  {
+    text: '111',
+    value: '111',
+    id: '112'
+  },
+  {
+    text: '222',
+    value: '222',
+    id: '222'
+  },
+  {
+    text: '333',
+    value: '333',
+    id: '333'
+  },
+  {
+    text: '444',
+    value: '444',
+    id: '444'
+  },
+  {
+    text: '555',
+    value: '555',
+    id: '555'
+  },
+  {
+    text: '666',
+    value: '666',
+    id: '666'
+  },
+  {
+    text: '777',
+    value: '777',
+    id: '777'
+  },
+  {
+    text: '888',
+    value: '888',
+    id: '888'
+  },
+  {
+    text: '999',
+    value: '999',
+    id: '999'
+  }
+]
+
+const cascadeData = [
+  {
+    value: 'Fruit',
+    text: 'Fruit',
+    children: [
+      {
+        value: 'Apple',
+        text: 'Apple',
+        children: [
+          {
+            value: 1,
+            text: 'One'
+          },
+          {
+            value: 2,
+            text: 'Two'
+          }
+        ]
+      },
+      {
+        value: 'Orange',
+        text: 'Orange',
+        children: [
+          {
+            value: 3,
+            text: 'Three'
+          },
+          {
+            value: 4,
+            text: 'Four'
+          }
+        ]
+      },
+      {
+        value: 'Lemon',
+        text: 'Lemon',
+        children: [
+          {
+            value: 1,
+            text: 'One'
+          },
+          {
+            value: 2,
+            text: 'Two'
+          },
+          {
+            value: 3,
+            text: 'Three'
+          },
+          {
+            value: 4,
+            text: 'Four'
+          },
+          {
+            value: 5,
+            text: 'Five'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    value: 'Drink',
+    text: 'Drink',
+    children: [
+      {
+        value: 'Coffee',
+        text: 'Coffee',
+        children: [
+          {
+            value: 1,
+            text: 'One'
+          },
+          {
+            value: 2,
+            text: 'Two'
+          },
+          {
+            value: 3,
+            text: 'Three'
+          }
+        ]
+      },
+      {
+        value: 'Tea',
+        text: 'Tea',
+        children: [
+          {
+            value: 1,
+            text: 'One'
+          },
+          {
+            value: 2,
+            text: 'Two'
+          },
+          {
+            value: 3,
+            text: 'Three'
+          },
+          {
+            value: 4,
+            text: 'Four'
+          }
+        ]
+      },
+      {
+        value: 'Juice',
+        text: 'Juice',
+        children: [
+          {
+            value: 1,
+            text: 'One'
+          },
+          {
+            value: 2,
+            text: 'Two'
+          },
+          {
+            value: 3,
+            text: 'Three'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    value: 'Dessert',
+    text: 'Dessert',
+    children: [
+      {
+        value: 'Chocolate',
+        text: 'Chocolate',
+        children: [
+          {
+            value: 1,
+            text: 'One'
+          },
+          {
+            value: 2,
+            text: 'Two'
+          },
+          {
+            value: 3,
+            text: 'Three'
+          },
+          {
+            value: 4,
+            text: 'Four'
+          },
+          {
+            value: 5,
+            text: 'Five'
+          }
+        ]
+      },
+      {
+        value: 'cheese',
+        text: 'cheese',
+        children: [
+          {
+            value: 1,
+            text: 'One'
+          },
+          {
+            value: 2,
+            text: 'Two'
+          },
+          {
+            value: 3,
+            text: 'Three'
+          },
+          {
+            value: 4,
+            text: 'Four'
+          }
+        ]
+      },
+      {
+        value: 'cake',
+        text: 'cake',
+        children: [
+          {
+            value: 1,
+            text: 'One'
+          },
+          {
+            value: 2,
+            text: 'Two'
+          },
+          {
+            value: 3,
+            text: 'Three'
+          }
+        ]
+      }
+    ]
+  }
+]
+
+export {
+  pickerData1,
+  pickerData2,
+  pickerData3,
+  cascadeData
+}
